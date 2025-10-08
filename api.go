d811f7a653c44f70f043bc2713c8578de932bcf8
Hao Wang <wh.tenghe@gmail.com>
2025-10-08 22:00:06 +1100

    feat: add database to the project, frontend display data in users table

--- api.go
+++ api.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"html/template"
+	"log"
+	"net/http"
+
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	InitDB()
+	defer DB.Close()
+
 	r := gin.Default()
+
 	r.Static("/static", "./static")
 
-	tmpl := template.Must(template.ParseGlob("templates/*.html"))
-	r.SetHTMLTemplate(tmpl)
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{
-			"Title": "Go + Gin + TailwindCSS",
-			"Body":  "This is rendered using Gin with Go templates and Tailwind!",
-		})
+		rows, err := DB.Query("SELECT id, name, email, age FROM users")
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+		defer rows.Close()
+
+		var users []User
+		for rows.Next() {
+			var u User
+			if err := rows.Scan(&u.ID, &u.Name, &u.Email, &u.Age); err != nil {
+				c.String(http.StatusInternalServerError, err.Error())
+				return
+			}
+			users = append(users, u)
+		}
+
+		c.Status(http.StatusOK)
+		tmpl.Execute(c.Writer, users)
 	})
 
+	log.Println("Server started at http://localhost:8081")
 	r.Run(":8081")
 }
---
+++ database.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	var err error
+	DB, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("Error opening DB:", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal("Error connecting to DB:", err)
+	}
+
+	fmt.Println("✅ Connected to PostgreSQL successfully!")
+}
